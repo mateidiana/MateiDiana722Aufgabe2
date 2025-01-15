434d80969ac9043c67de33a3f5fe87baafd163f1
mateidiana <mateidiana10@gmail.com>
2025-01-15 09:30:36 +0200

    initialised generic classes for 3 layer

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,7 @@
 package org.example;
+import org.example.model.*;
+import org.example.repo.IRepository;
+import org.example.repo.InMemoryRepository;
 
 public class Main {
     public  static void main(String[] args){
